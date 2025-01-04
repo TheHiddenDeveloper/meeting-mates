7ab58230243d0390ea8fa73ce034be10ffe3e62e
Rodney (K.D) Hagan <rodneyhagan74@gmail.com>
2025-01-04 07:23:58 +0000

    Seperated backend from frontend codes

--- backend/routes/availability.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAvailableSlots } = require('../controllers/availability');
-
-// Fetch available time slots for a user
-router.get('/:userId/available-slots', getAvailableSlots);
-
-module.exports = router;
